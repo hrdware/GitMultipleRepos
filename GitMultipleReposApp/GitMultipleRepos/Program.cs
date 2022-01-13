c6b64dbc36393c0c48b2ba246be40b8844b09524
wtebo <wtebo@uco.edu>
2022-01-13 06:58:00 -0600

    Retrieving a list of subdirectories and verifying each one is a git repository.  If it is not a repository, it is removed from the list.

--- GitMultipleReposApp/GitMultipleRepos/Program.cs
+++ GitMultipleReposApp/GitMultipleRepos/Program.cs
@@ -8,3 +8,34 @@
 // Display a list of warnings any time the specified folder does not exist or the batch file within that folder does not exist
 //
 // Eventually, the application could take in the parent directory as a parameter or a WPF UI could be added.
+
+using System.Diagnostics;
+
+List<string> DirectoryList = new List<string>();
+
+DirectoryList = GetDirectoryList();
+for(int i=DirectoryList.Count-1; i>=0; i--) 
+{
+    string fqdn = Path.Combine(DirectoryList[i], ".git");
+    if(!Directory.Exists(fqdn)) // directory is not a versioned directory
+    {
+        Console.WriteLine($"Removing Directory { DirectoryList[i] }");
+        DirectoryList.RemoveAt(i);
+    }
+}
+Console.WriteLine($"There are { DirectoryList.Count } directories reamining in the list.");
+Console.ReadLine();
+
+static List<string> GetDirectoryList()
+{
+    string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+    location = Path.GetDirectoryName(location);
+    string[] dirs = Directory.GetDirectories(location);
+    return dirs.ToList();
+}
+
+
+
+
+
+
